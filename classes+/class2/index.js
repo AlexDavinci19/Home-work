f19cee7c96863c1ab05726d9b56ef1944467d732
AlexDavinci19 <de.krolzzz@gmail.com>
2022-11-29 00:50:26 +0500

    m

---
+++ classes+/class2/index.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    compareAge(person) {
+        if (this.age > person.age) {
+            alert(`${this.name} старше, чем ${person.name}`);
+        } else {
+            alert(`${this.name} младше, чем ${person.name}`);
+        }
+    }
+}
+const person1 = new Person('Максим', 24);
+const person2 = new Person('Светлана', 36);
+const person3 = new Person('Ирина', 23);
+
+
+person1.compareAge(person2);
+person2.compareAge(person3);
+person3.compareAge(person1);
